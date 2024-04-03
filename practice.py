2952c92d28e07647e4295f7e2dd4ac68249470ee
sande <sandesanthosh88@gmail.com>
2024-04-03 18:46:39 +0530

    sec

--- practice.py
+++ practice.py
@@ -2,8 +2,24 @@
 # print(1+'a') #error
 # print('a'+'a') #aa
 
-var = "Santhosh"
-up_var =var.upper()
-print(up_var)
-print(up_var.lower())
-print(len(var))
+# var = "Santhosh"
+# up_var =var.upper()
+# print(up_var)
+# print(up_var.lower())
+# print(len(var))
+
+# a=[]
+# b=a.append()
+# print(a)
+
+a="welcome to azure family"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a[1:3])
+print(a[4:9])
+print(a[11:12])
+print(a[:3])
+print(a[4:])
+print(a[1:10:2])
+print(a[:10:3])
