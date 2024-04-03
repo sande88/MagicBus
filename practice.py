393c09cc78d5d66489555e54859e0c8eef37bfd7
sande <sandesanthosh88@gmail.com>
2024-04-03 12:20:27 +0530

    commit 2

--- practice.py
+++ practice.py
@@ -1,3 +1,9 @@
-print(1+1) #2
-print(1+'a') #error
-print('a'+'a') #aa
+# print(1+1) #2
+# print(1+'a') #error
+# print('a'+'a') #aa
+
+var = "Santhosh"
+up_var =var.upper()
+print(up_var)
+print(up_var.lower())
+print(len(var))
