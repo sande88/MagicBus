223c9a18156cc4985f49d3bb710aeacc11bb56d6
sande <sandesanthosh88@gmail.com>
2024-04-05 12:12:37 +0530

    second

--- practice.py
+++ practice.py
@@ -12,14 +12,19 @@
 # b=a.append()
 # print(a)
 
-a="welcome to azure family"
-print(len(a))
-print(a.upper())
-print(a.lower())
-print(a[1:3])
-print(a[4:9])
-print(a[11:12])
-print(a[:3])
-print(a[4:])
-print(a[1:10:2])
-print(a[:10:3])
+# a="welcome to azure family"
+# a='new'
+# print(a)
+# print(len(a))
+# print(a.upper())
+# print(a.lower())
+# print(a[1:3])
+# print(a[4:9])
+# print(a[11:12])
+# print(a[:3])
+# print(a[4:])
+# print(a[1:10:2])
+# print(a[:10:3])
+strin=''
+# print(strin[::-1])
+
