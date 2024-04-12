e1a481055fcabac0c519d278d1a6c2ffcf555df2
sande <sandesanthosh88@gmail.com>
2024-04-12 12:25:03 +0530

    second commit

--- practice.py
+++ practice.py
@@ -25,6 +25,40 @@
 # print(a[4:])
 # print(a[1:10:2])
 # print(a[:10:3])
-strin=''
+# strin=''
 # print(strin[::-1])
+# def prime(num):
+
+#     flag = False
+
+#     if num == 1:
+#         print(num, "is not a prime number")
+#     elif num > 1:
+#     # check for factors
+#         for i in range(2, num):
+#             if (num % i) == 0:
+#             # if factor is found, set flag to True
+#                 flag = True
+#             # break out of loop
+#                 break
+
+#         if flag:
+#             print(num, "is not a prime number")
+#         else:
+#             print(num, "is a prime number")
+
+
+
+# prime(5)
+
+a=[]
+n=int(input("enter a number"))
+for i in range(n):
+    username=input(f"username {i}")
+    a.append(username)
+
+print(a)
+
+
+
 
